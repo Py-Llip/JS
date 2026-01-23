8b224308bb7664101908349f76ea3abe14956fda
FellipeAlvesBritoOficial <fellipealvesbrito09@gmail.com>
2026-01-23 20:45:46 -0300

    feito a primeira parte do exercicio da aula 12

---
+++ D-condicoes/aula12ex/modelo/script.js
@@ -0,0 +1,10 @@
+var hora = new Date().getHours()
+
+document.querySelector('section div').innerText += ` ${hora} horas`
+if (hora < 12) {
+    // document.getElementsByTagName('img')
+} else if (hora < 18) {
+
+} else {
+
+}
