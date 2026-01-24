2759dae7bfbe1ae996c3aefca3a963b3038cc1ff
FellipeAlvesBritoOficial <fellipealvesbrito09@gmail.com>
2026-01-23 21:35:58 -0300

    feito o exercicio 01 da aula 12

--- D-condicoes/aula12ex/ex014/script.js
+++ D-condicoes/aula12ex/ex014/script.js
@@ -1,10 +1,20 @@
-var hora = new Date().getHours()
-
-document.querySelector('section div').innerText += ` ${hora} horas`
-if (hora < 12) {
-    // document.getElementsByTagName('img')
-} else if (hora < 18) {
-
-} else {
-
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src = 'manha.png'
+        document.body.style.backgroundColor = '#e2cd9f'
+    } else if (18 >= hora && hora >= 12) {
+        // boa tarde
+        img.src = 'tarde.png'
+        document.body.style.backgroundColor = '#b9846f'
+    } else {
+        // boa noite
+        img.src = 'noite.png'
+        document.body.style.backgroundColor = '#515154'
+    }
 }
